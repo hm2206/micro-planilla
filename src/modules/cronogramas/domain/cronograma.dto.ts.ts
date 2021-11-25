e54236ff2245ce54c09fa284870c9b57afd11961
Hans <twd2206@gmail.com>
2021-11-24 21:57:23 -0500

    add options

--- src/modules/cronogramas/domain/cronograma.dto.ts.ts
+++ src/modules/cronogramas/domain/cronograma.dto.ts.ts
@@ -1,12 +1,15 @@
-import { IsNumber } from 'class-validator';
+import { IsNumber, IsOptional } from 'class-validator';
 
 export class FilterTypeObject {
+  @IsOptional()
   @IsNumber()
   public cargoId?: number
 
+  @IsOptional()
   @IsNumber()
   public typeCategoriaId?: number
 
+  @IsOptional()
   @IsNumber()
   public metaId?: number
 }
